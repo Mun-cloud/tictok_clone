ddbf4c424f31ce40b974e10221a0c1ef470a61e7
문태호(Mun TaeHo) <mun05170@naver.com>
2023-07-01 13:45:52 +0900

    sign up screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/authentication/sign_up_screen.dart';
 
 void main() {
-  runApp(const TicTokApp());
+  runApp(const TikTokApp());
 }
 
-class TicTokApp extends StatelessWidget {
-  const TicTokApp({super.key});
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "TikTok Clone",
+      title: 'TikTok Clone',
       theme: ThemeData(
-        primaryColor: const Color(0xffe9435a),
+        primaryColor: const Color(0xFFE9435A),
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: const AppBarTheme(
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.white,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            color: Colors.black,
+            fontSize: Sizes.size16 + Sizes.size2,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
-      home: Container(),
+      home: const SignUpScreen(),
     );
   }
 }
