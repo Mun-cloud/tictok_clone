a0ec68d5116d44ec7f08e7fff024b59a0fb2efb1
Taeho <mun05170@naver.com>
2023-07-06 19:15:59 +0900

    interests screen 9:35

--- lib/features/authentication/login_form_screen.dart
+++ lib/features/authentication/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 import 'package:tictok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tictok_clone/features/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -12,54 +13,83 @@ class LoginFormScreen extends StatefulWidget {
 
 class _LoginFormScreenState extends State<LoginFormScreen> {
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-
+  Map<String, String> formData = {};
   void _onSubmitTap() {
     if (_formkey.currentState != null) {
       if (_formkey.currentState!.validate()) {
         _formkey.currentState!.save();
+
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsScreen(),
+          ),
+        );
       }
     }
   }
 
+  void _onScaffoldTap() {
+    FocusScope.of(context).unfocus();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Log in"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: Sizes.size36,
+    return GestureDetector(
+      onTap: _onScaffoldTap,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Log in"),
         ),
-        child: Form(
-            key: _formkey,
-            child: Column(
-              children: [
-                Gaps.v28,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Email",
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size36,
+          ),
+          child: Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  Gaps.v28,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Email",
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "이메일을 입력해주세요.";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData["email"] = newValue;
+                      }
+                    },
                   ),
-                  validator: (value) {
-                    return "I dont like your email";
-                  },
-                ),
-                Gaps.v16,
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Password",
+                  Gaps.v16,
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                    validator: (value) {
+                      if (value != null && value.isEmpty) {
+                        return "비밀번호를 입력해 주세요.";
+                      }
+                      return null;
+                    },
+                    onSaved: (newValue) {
+                      if (newValue != null) {
+                        formData["password"] = newValue;
+                      }
+                    },
                   ),
-                  validator: (value) {
-                    return "잘못된 비번";
-                  },
-                ),
-                Gaps.v28,
-                GestureDetector(
-                  onTap: _onSubmitTap,
-                  child: const FormButton(disabled: false),
-                )
-              ],
-            )),
+                  Gaps.v28,
+                  GestureDetector(
+                    onTap: _onSubmitTap,
+                    child: const FormButton(disabled: false),
+                  )
+                ],
+              )),
+        ),
       ),
     );
   }
