3cdf02e145af706026f6d6d18c91e3c04e6f03cb
Taeho <mun05170@naver.com>
2023-07-12 18:16:50 +0900

    stateful navigation part two

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,10 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/features/main_navigation/widgets/nav_tab.dart';
 
-class MainNavigationScreen extends StatelessWidget {
+class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
 
   @override
+  State<MainNavigationScreen> createState() => _MainNavigationScreenState();
+}
+
+class _MainNavigationScreenState extends State<MainNavigationScreen> {
+  int _selectedIndex = 0;
+
+  final screens = [
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Home"),
+    ),
+    Container(),
+    const Center(
+      child: Text("Home"),
+    ),
+    const Center(
+      child: Text("Search"),
+    )
+  ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: screens.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                icon: FontAwesomeIcons.house,
+                isSelected: _selectedIndex == 0,
+                selectedIcon: FontAwesomeIcons.house,
+                text: "Home",
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.compass,
+                isSelected: _selectedIndex == 1,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                text: "Discover",
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.message,
+                isSelected: _selectedIndex == 3,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                text: "Inbox",
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.user,
+                isSelected: _selectedIndex == 4,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                text: "Profile",
+                onTap: () => _onTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
