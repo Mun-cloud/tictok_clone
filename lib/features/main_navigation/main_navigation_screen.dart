539189bb5c90a651f9c079fbeb2e0a1374099bc2
Taeho <mun05170@naver.com>
2023-07-13 18:33:01 +0900

    VideoPlayer

--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/features/main_navigation/widgets/nav_tab.dart';
+import 'package:tictok_clone/features/videos/video_timeline_screen.dart';
+
+import 'widgets/post_video_button.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -14,7 +18,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   final screens = [
     const Center(
-      child: Text("Home"),
+      child: VideoTimelineScreen(),
     ),
     const Center(
       child: Text("Home"),
@@ -34,6 +38,17 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
     });
   }
 
+  void _onPostVideoButtonTap() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Record video"),
+        ),
+      ),
+      fullscreenDialog: true,
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,6 +59,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
           padding: const EdgeInsets.all(12),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            // crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               NavTab(
                 icon: FontAwesomeIcons.house,
@@ -59,6 +75,12 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
                 text: "Discover",
                 onTap: () => _onTap(1),
               ),
+              Gaps.h24,
+              GestureDetector(
+                onTap: _onPostVideoButtonTap,
+                child: const PostVideoButton(),
+              ),
+              Gaps.h24,
               NavTab(
                 icon: FontAwesomeIcons.message,
                 isSelected: _selectedIndex == 3,
