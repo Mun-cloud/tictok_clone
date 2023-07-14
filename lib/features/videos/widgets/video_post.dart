e4633392c347f01e4414541016e3559a739840d6
Taeho <mun05170@naver.com>
2023-07-14 14:33:42 +0900

    Video UI 5:09

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tictok_clone/constants/gaps.dart';
 import 'package:tictok_clone/constants/sizes.dart';
 import 'package:video_player/video_player.dart';
 import 'package:visibility_detector/visibility_detector.dart';
@@ -17,9 +18,13 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video2.mp4");
+  bool _isPause = false;
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+  late final AnimationController _animationController;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -32,15 +37,25 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    _videoPlayerController.play();
-    setState(() {});
+    await _videoPlayerController.setLooping(true);
     _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
     _initVideoPlayer();
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+    _animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   @override
@@ -52,18 +67,20 @@ class _VideoPostState extends State<VideoPost> {
   void _onVisibilityChanged(VisibilityInfo info) {
     if (info.visibleFraction == 1 && !_videoPlayerController.value.isPlaying) {
       _videoPlayerController.play();
-    } else {
-      // _videoPlayerController.pause();
     }
   }
 
   void _onTogglePause() {
     if (_videoPlayerController.value.isPlaying) {
       _videoPlayerController.pause();
+      _animationController.reverse();
     } else {
       _videoPlayerController.play();
+      _animationController.forward();
     }
-    setState(() {});
+    setState(() {
+      _isPause = !_isPause;
+    });
   }
 
   @override
@@ -88,16 +105,50 @@ class _VideoPostState extends State<VideoPost> {
           Positioned.fill(
             child: IgnorePointer(
               child: Center(
-                child: FaIcon(
-                  _videoPlayerController.value.isPlaying
-                      ? FontAwesomeIcons.play
-                      : FontAwesomeIcons.pause,
-                  color: Colors.white,
-                  size: Sizes.size52,
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) => Transform.scale(
+                    scale: _animationController.value,
+                    child: child,
+                  ),
+                  child: AnimatedOpacity(
+                    opacity: _isPause ? 1 : 0,
+                    duration: _animationDuration,
+                    child: const FaIcon(
+                      FontAwesomeIcons.play,
+                      color: Colors.white,
+                      size: Sizes.size52,
+                    ),
+                  ),
                 ),
               ),
             ),
           ),
+          const Positioned(
+            bottom: 30,
+            left: 30,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "@Taeho",
+                  style: TextStyle(
+                    fontSize: Sizes.size20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Gaps.v10,
+                Text(
+                  "This is my house in Thailand!!!",
+                  style: TextStyle(
+                    fontSize: Sizes.size16,
+                    color: Colors.white,
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
