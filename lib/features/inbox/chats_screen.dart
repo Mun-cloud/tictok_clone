7b218c7deef065ed64fe617f17fda851f88024f8
Taeho <mun05170@naver.com>
2023-07-21 14:29:55 +0900

    Chat Detail

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tictok_clone/constants/sizes.dart';
+import 'package:tictok_clone/features/inbox/chat_detail_screen.dart';
 
 class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
@@ -12,13 +13,68 @@ class ChatsScreen extends StatefulWidget {
 class _ChatsScreenState extends State<ChatsScreen> {
   final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
   final List<int> _items = [];
+
+  final Duration _duration = const Duration(milliseconds: 300);
+
   void _addItem() {
     if (_key.currentState != null) {
-      _key.currentState!.insertItem(
-        _items.length,
-      );
+      _key.currentState!.insertItem(_items.length, duration: _duration);
       _items.add(_items.length);
     }
+  }
+
+  void _onDeleteItem(int index) {
+    if (_key.currentState != null) {
+      _key.currentState!.removeItem(
+        index,
+        (context, animation) => SizeTransition(
+          sizeFactor: animation,
+          child: Container(color: Colors.red, child: _makeTile(index)),
+        ),
+        duration: _duration,
+      );
+      _items.removeAt(index);
+    }
+  }
+
+  void _onChatTap() {
+    Navigator.of(context).push(MaterialPageRoute(
+      builder: (context) => const ChatDetailScreen(),
+    ));
+  }
+
+  Widget _makeTile(int index) {
+    return ListTile(
+      onLongPress: () => _onDeleteItem(index),
+      onTap: _onChatTap,
+      key: UniqueKey(),
+      leading: const CircleAvatar(
+        radius: 30,
+        foregroundImage: NetworkImage(
+            "https://avatars.githubusercontent.com/u/85348363?v=4"),
+        child: Text("Taeho"),
+      ),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            "Baaam ($index)",
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          Text(
+            "2:16 PM",
+            style: TextStyle(
+              color: Colors.grey.shade500,
+              fontSize: Sizes.size12,
+            ),
+          ),
+        ],
+      ),
+      subtitle: const Text("Don't forget to make video"),
+    );
   }
 
   @override
@@ -43,38 +99,7 @@ class _ChatsScreenState extends State<ChatsScreen> {
         ),
         itemBuilder: (context, index, animation) => SizeTransition(
           sizeFactor: animation,
-          child: FadeTransition(
-            opacity: animation,
-            child: ListTile(
-              key: UniqueKey(),
-              leading: const CircleAvatar(
-                radius: 30,
-                foregroundImage: NetworkImage(
-                    "https://avatars.githubusercontent.com/u/85348363?v=4"),
-                child: Text("Taeho"),
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    "Baaam ($index)",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    "2:16 PM",
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontSize: Sizes.size12,
-                    ),
-                  ),
-                ],
-              ),
-              subtitle: const Text("Don't forget to make video"),
-            ),
-          ),
+          child: FadeTransition(opacity: animation, child: _makeTile(index)),
         ),
       ),
     );
